0494fcea1ce243638ded34c27a94607a7cb93e61
nsd-design <ibayo427@gmail.com>
2025-02-08 22:13:44 +0000

    Create PrixService, define prix default value

--- salon/urls.py
+++ salon/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from salon.views import services, add_category, add_service
+from salon.views import *
 
 urlpatterns = [
     path("services/", services, name="services"),
     path("services/add/", add_service, name="add_service"),
     path("categorie/add/", add_category, name="add_categorie"),
+    path("prix/", prix_services, name="prix_services"),
+    path("prix/add/", add_prix_service, name="add_prix_services"),
 ]
