ba91a6a80d7d3bd5631eff245b62ad78115dbf6d
nsd-design <ibayo427@gmail.com>
2025-02-08 17:37:07 +0000

    Create and Display Service

--- salon/urls.py
+++ salon/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from salon.views import services, add_category
+from salon.views import services, add_category, add_service
 
 urlpatterns = [
     path("services/", services, name="services"),
+    path("services/add/", add_service, name="add_service"),
     path("categorie/add/", add_category, name="add_categorie"),
 ]
