327409d4717834f61f881968d326261728e9b56d
nsd-design <ibayo427@gmail.com>
2025-07-19 19:05:55 +0000

    Statistiques sur la vente des Produits du Salon

--- employe/views.py
+++ employe/views.py
@@ -1,8 +1,10 @@
+import http
 import json
 from datetime import date, timedelta
 
 import django.db.utils
 from django.db import IntegrityError
+from django.db.models import Sum, Q
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.template.loader import render_to_string
@@ -11,6 +13,7 @@ from django.views.decorators.http import require_http_methods
 
 from employe.forms import EmployeForm
 from employe.models import Employe
+from salon.models import Vente, Depense
 
 tmp_base = "employe/"
 
@@ -108,3 +111,75 @@ def add_employe(request):
             if 'employe_employe_email_key' in str(e):
                 return JsonResponse({"error": "Ce email existe déjà."}, status=409)
             return JsonResponse({"error": "Echec de l'enregistrement pour des raison de doublon"}, status=409)
+
+
+def get_stats_vente_produits(annee: int, num_week: int):
+    try:
+        ventes = Vente.objects.filter(
+            created_at__date__year=annee,
+            created_at__date__week=num_week,
+        )
+        # Nombre de Ventes réalisé
+        nb_ventes = ventes.count()
+        sum_montant_ventes = ventes.aggregate(Sum("montant_total"))['montant_total__sum'] or 0
+        sum_remise_ventes = ventes.aggregate(Sum("reduction"))['reduction__sum'] or 0
+
+        total_net_ventes = sum_montant_ventes - sum_remise_ventes
+
+        # Nombre de Ventes en Cash
+        nb_ventes_cash = ventes.filter(type_vente=1).count()
+
+        # Montant total de Ventes en Cash
+        resultats_ventes_cash = ventes.aggregate(
+            sum_vente_cash=Sum("montant_total", filter=Q(type_vente=1)),
+            sum_remise_vente_cash=Sum("reduction", filter=Q(type_vente=1)),
+        )
+        sum_ventes_cash = resultats_ventes_cash['sum_vente_cash'] or 0
+        sum_remise_ventes_cash = resultats_ventes_cash['sum_remise_vente_cash']
+
+        total_ventes_cash = sum_ventes_cash - sum_remise_ventes_cash
+
+
+        # Nombre de Ventes à Crédit
+        nb_ventes_credits = ventes.filter(type_vente=2).count()
+
+        # Montant total de Ventes à Crédit
+        resultats_ventes_credits = ventes.aggregate(
+            total_montant_credit=Sum("montant_total", filter=Q(type_vente=2)),
+            total_remise_credit=Sum("reduction", filter=Q(type_vente=2))
+        )
+        sum_ventes_credit = resultats_ventes_credits["total_montant_credit"] or 0
+        sum_remise_ventes_credit = resultats_ventes_credits["total_remise_credit"] or 0
+
+        total_ventes_credits = sum_ventes_credit - sum_remise_ventes_credit
+
+        response = {
+            "nb_ventes": nb_ventes,
+            "sum_montant_ventes": sum_montant_ventes,
+            "sum_remise_ventes": sum_remise_ventes,
+            "total_net_ventes": total_net_ventes,
+            "nb_ventes_cash": nb_ventes_cash,
+            "sum_ventes_cash": sum_ventes_cash,
+            "sum_remise_ventes_cash": sum_remise_ventes_cash,
+            "total_ventes_cash": total_ventes_cash,
+            "nb_ventes_credits": nb_ventes_credits,
+            "sum_ventes_credit": sum_ventes_credit,
+            "sum_remise_ventes_credit": sum_remise_ventes_credit,
+            "total_ventes_credits": total_ventes_credits,
+        }
+        return response
+    except Vente.DoesNotExist:
+        return None
+
+@require_http_methods(["POST"])
+def filtre_dashmin_data(request):
+    try:
+        data = json.loads(request.body)
+
+        stats_vente_produits = get_stats_vente_produits(int(data['year']), int(data['week_num']))
+        data = {
+            "stats_vente_produits": stats_vente_produits,
+        }
+        return JsonResponse({"success": True, "data": data}, status=http.HTTPStatus.FOUND)
+    except json.JSONDecodeError:
+        return JsonResponse({"error": True, "msg": "Format de données invalides"}, status=400)
