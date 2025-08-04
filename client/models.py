b67046bebc6c3d8ea93a026b9024e8b314884382
nsd-design <ibayo427@gmail.com>
2025-08-04 22:28:11 +0000

    Update des models Reservation et Location 04-08-2025

--- client/models.py
+++ client/models.py
@@ -14,23 +14,55 @@ class Client(MyBaseModel):
         return f'{self.nom_complet} - {self.telephone}'
 
 
+class ZoneAReserver(MyBaseModel):
+    STATUT_CHOICES = [('libre', "LIBRE"), ('reserve', "RESERVEE")]
+
+    nom = models.CharField(max_length=100)
+    statut = models.CharField(max_length=20, choices=STATUT_CHOICES, default='libre')
+
+    def __str__(self):
+        return f"Zone: {self.nom} - {self.statut}"
+
 class Reservation(MyBaseModel):
     TYPE = [(1, "Service"), (2, "Evénement")]
-    ZONE_RESERVE = [(1, "Salon"), (2, "Piscine"), (3, "Local")]
-    ETAT_RESERVATION = [(1, "En cours"), (2, "Terminée"), (3, "Annulée"), ]
+    ETAT_RESERVATION = [(1, "En cours"), (2, "Terminée"), (3, "Annulée"), (4, "En attente"), (5, "Confirmée")]
 
-    type = models.SmallIntegerField(choices=TYPE, default=1)
+    type = models.SmallIntegerField(choices=TYPE)
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
-    zone = models.SmallIntegerField(choices=ZONE_RESERVE)
+    zone = models.ForeignKey(ZoneAReserver, on_delete=models.CASCADE)
     etat_reservation = models.SmallIntegerField(choices=ETAT_RESERVATION)
+    date_debut = models.DateField()
+    date_fin = models.DateField()
+    commentaire = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.zone.nom}"
 
 
 class Location(MyBaseModel):
+    STATUT_LOCATION = [
+        ('en_attente', 'En attente'),
+        ('confirmee', 'Confirmée'),
+        ('en_cours', 'En cours'),
+        ('terminee', 'Terminée'),
+        ('annulee', 'Annulée'),
+        ('expiree', 'Expirée'),
+    ]
+    TYPE = [(1, "Service"), (2, "Evénement")]
+
     locateur = models.ForeignKey(Client, on_delete=models.CASCADE)
     objet = models.CharField(max_length=120, null=True)
-    description = models.CharField(max_length=255, null=True)
+    zone = models.ForeignKey(ZoneAReserver, on_delete=models.CASCADE)
+    description = models.TextField(null=True, blank=True)
     montant_a_payer = models.BigIntegerField()
     montant_reduit = models.BigIntegerField(null=True)
+    date_debut = models.DateField()
+    date_fin = models.DateField()
+    statut = models.CharField(max_length=20, choices=STATUT_LOCATION, default='en_attente')
+    type_location = models.SmallIntegerField(choices=TYPE)
+
+    def __str__(self):
+        return f"{self.zone.nom} - {self.locateur.nom_complet} - Début: {self.date_debut}, Fin: {self.date_fin}"
 
 
 class Piscine(MyBaseModel):
